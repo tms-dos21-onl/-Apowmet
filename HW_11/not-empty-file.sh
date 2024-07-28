1507b415e715bbadaf1386efd337ef036391e910
Aleksey Ivanishchev <4ai6ezcaxapa@gmail.com>
2024-07-28 12:29:55 +0300

    update3

--- HW_11/not-empty-file.sh
+++ HW_11/not-empty-file.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 while read -r line; do
-    if [[ -e "$line" ]]; then
-        if [[ -s "$line" ]]; then
-            echo "$line"
-        else
-            echo "Size is equal 0 bytes"
-        fi
-        exit 0
-    else
-        echo "$line does not exist"
-    fi
-    exit 0
+	if [[ -e "$line" ]]; then
+		if [[ -s "$line" ]]; then
+			echo "$line"
+		else
+			echo "Size is equal 0 bytes"
+		fi
+		exit 0
+	else
+		echo "$line does not exist"
+	fi
+	exit 0
 done
